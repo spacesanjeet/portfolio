5dc5d7b3327d57b652a1b22d69dd625ebf04fef9
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2023-06-17 21:55:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-// JavaScript code for dark mode toggle
-
 // Check if the user has a preferred color scheme
 const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 // Get the dark mode toggle element
 const darkModeToggle = document.getElementById('darkModeToggle');
+const menuIcon = document.getElementById('menuIcon');
+const menu = document.getElementById('menu');
 
 // Function to toggle dark mode
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
+}
+
+// Function to toggle menu
+function toggleMenu() {
+  menu.classList.toggle('active');
 }
 
 // Initialize dark mode based on user preference
@@ -20,4 +25,9 @@ if (prefersDarkMode) {
 // Add event listener to the dark mode toggle
 darkModeToggle.addEventListener('change', () => {
   toggleDarkMode();
+});
+
+// Add event listener to the menu icon
+menuIcon.addEventListener('click', () => {
+  toggleMenu();
 });
